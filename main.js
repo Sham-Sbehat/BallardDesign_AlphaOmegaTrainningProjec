989efef3880080c3f7f6f301cc91e4aa20219417
Sham Sbehat <121432620+Sham-Sbehat@users.noreply.github.com>
2025-05-02 02:54:49 +0300

    mainSection

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+// For Navbar responsive
 function toggleMenu() {
-    document.querySelector('.icons.menu-icons').classList.toggle('show');
-  }
+  document.querySelector(".icons.menu-icons").classList.toggle("show");
+}
+
+// For wall-art-section data fetching
+fetch("./data/categoriesData.json")
+  .then((response) => response.json())
+  .then((data) => {
+    const container = document.getElementById("wall-art-section");
+    const content = data
+      .map((item) => {
+        return `
+        <div class="art-item">
+          <img src="${item.image}" alt="${item.title}" class="image">
+          <a href="#" class="imageTitle">${item.title}</a>
+        </div>
+      `;
+      })
+      .join("");
+    container.innerHTML = content;
+  })
+  .catch((error) => {
+    console.error("Error loading JSON:", error);
+  });
